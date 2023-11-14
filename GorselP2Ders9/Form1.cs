431d488a1a2339fd300a49b80125761917fa700a
Doğukan ÖZEL <dogukanozel692@gmail.com>
2023-11-14 23:41:45 +0300

    SQLLogin

--- GorselP2Ders9/Form1.cs
+++ GorselP2Ders9/Form1.cs
@@ -43,7 +43,7 @@ namespace GorselP2Ders9
             }
             else
             {
-                MessageBox.Show("Kullanıcı Adı veya Şifre YANLIŞ!!!!");
+                MessageBox.Show("Kullanıcı Adı veya Şifre YANLIŞ!!!");
             }
 
         }
